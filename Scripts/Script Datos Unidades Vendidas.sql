43ee1d7a8a48d1ce8fc098d348231fe43ee854cd
Zagok <gomezcarlos1524@outlook.com>
2021-09-14 11:15:07 -0500

    COMMIT INSERTAR DATOS UNIDADESV

---
+++ Scripts/Script Datos Unidades Vendidas.sql
@@ -0,0 +1,4 @@
+insert into unidadesvendidas values('VWCAR2020GOLST', 'GOL 2020');
+insert into unidadesvendidas values('VWCAR2020POLOAUT', 'POLO 2020');
+insert into unidadesvendidas values('VWCAR2020VENTOST', 'VENTO 2020');
+insert into unidadesvendidas values('VWCAR2020JETTAGLI', 'JETTA 2020');
