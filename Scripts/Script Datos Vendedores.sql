4fcb3b16341496d57ff6d573d464416d2aedd622
Zagok <gomezcarlos1524@outlook.com>
2021-09-14 10:50:13 -0500

    COMMIT INSERTAR DATOS VENDEDORES

---
+++ Scripts/Script Datos Vendedores.sql
@@ -0,0 +1,5 @@
+insert into Vendedores values('VWEM01', 'DE ALBA VILLALOBOS LILIANA LIZBETH');
+insert into Vendedores values('VWEM02', 'FACIO ESPINOSA JUAN CARLOS');
+insert into Vendedores values('VWEM03', 'GOMEZ CANO JOSE MIGUEL');
+insert into Vendedores values('VWEM04', 'GOMEZ CASTAÑEDA MARIA FERNANDA');
+insert into Vendedores values('VWEM05', 'GOMEZ LOPEZ RAFAEL');
